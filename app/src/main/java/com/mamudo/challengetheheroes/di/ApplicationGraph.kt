460fbf94a3f3363b3141e70237a822bc5b8ce132
Mahmut Taşkıran <taskiranmahmutt@gmail.com>
2020-12-15 22:30:19 +0300

    inject(MainActivity)

--- app/src/main/java/com/mamudo/challengetheheroes/di/ApplicationGraph.kt
+++ app/src/main/java/com/mamudo/challengetheheroes/di/ApplicationGraph.kt
@@ -1,10 +1,13 @@
 package com.mamudo.challengetheheroes.di
 
+import com.mamudo.challengetheheroes.MainActivity
+import com.mamudo.challengetheheroes.api.MarvelApi
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [NetworkModule::class])
 interface ApplicationGraph {
-
+    fun inject(a: MainActivity)
+    fun api(): MarvelApi
 }
