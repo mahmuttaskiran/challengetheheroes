3ddb225d215cc7bda56b42b6133a0a58b250882c
Mahmut Taşkıran <taskiranmahmutt@gmail.com>
2020-12-15 14:46:47 +0300

    TestApplicationGraph extension on ApplicationGraph (for test purposes)

---
+++ app/src/test/java/com/mamudo/challengetheheroes/di/TestApplicationGraph.kt
@@ -0,0 +1,11 @@
+package com.mamudo.challengetheheroes.di
+
+import com.mamudo.challengetheheroes.api.MarvelApiTest
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [NetworkModule::class])
+interface TestApplicationGraph: ApplicationGraph {
+    fun inject(test: MarvelApiTest)
+}
